aa7c960b0ac9cd24efdc9db5bfeecb24d27ad2ae
khalifa chelbi <80419535+khalifa-ch@users.noreply.github.com>
2022-12-04 09:35:29 +0100

    Update form_handler.php

--- form_handler.php
+++ form_handler.php
@@ -4,7 +4,7 @@ $visitor_email=$_POST['email'];
 $subject=$_POST['subject'];
 $message=$_POST['message'];
 
-$email_from='info@khalifa-ch.github.io/portfolio_website';
+$email_from='info@khalifa-ch.github.io';
 
 $email_subject='New Form submission';
 
