a8e955ccb65f689e22b604d8c3b281c5075d2324
khalifa-ch <khalifachelbi06@gmail.com>
2022-12-04 09:01:46 +0100

    first commit

---
+++ form_handler.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_POST['name'];
+$visitor_email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+$email_from='khalifaPortfolio.com';
+
+$email_subject='New Form submission';
+
+$email_body="User name  $name .\n".
+            "User email  $visitor_email .\n".  
+            " subject  $subject .\n".
+            "User Message  $message .\n";
+
+$to ='khalifachelbi06@gmail.com';
+
+$headers="From $email_from \r\n ";  
+
+$headers .="Reply-To $visitor_email \r\n ";
+mail($to,$email_subject,$email_body,$headers);
+header("location:contact.html");
+?>
